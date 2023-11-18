2c14119590363f1973a50c8532f56c2b3fd64cd0
Erchil66 <artecode66@gmail.com>
2023-11-18 13:47:05 +0800

    angle

--- packages/appinio_swiper/lib/appinio_swiper.dart
+++ packages/appinio_swiper/lib/appinio_swiper.dart
@@ -476,7 +476,7 @@ class _AppinioSwiperState extends State<AppinioSwiper>
     widget.onSwiping?.call(_calculateDirection(top: _top, left: _left));
     widget.listenAction!(
       _leftAnimation,
-      _difference,
+      _angle,
       _calculateDirection(top: _top, left: _left),
     );
   }
